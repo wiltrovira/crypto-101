dccfb9792f4a3e2f06dcc3fcb3a7a98b6a3ee784
Wilt Rovira <wrovira@gmail.com>
2017-09-04 23:14:54 -0500

    demostración de algoritmos completada

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var crypto = require("crypto");
-var hash = crypto.createHash("sha256");
+var data = "Escriba aquí su password";
 
-hash.on("readable", function(){
-	var data = hash.read();
-	if (data) {
-		console.log(data.toString("hex"));
-		// Prints:
-		//   6a2da20943931e9834fc12cfe5bb47bbd9ae43489a30726962b576f4e3993e50
-	}
-});
+//Opción 1 --  tiene vulnerabilidades
+var hash1 = crypto.createHash("md5").update(data, "utf-8").digest("hex");
+console.log("Algoritmo md5:   " + hash1);
 
-hash.write("Wilt rovira");
-hash.end();
+//Opción 2 -- No utilizar. Puede generar colisión
+var hash2 = crypto.createHash("sha1").update(data, "utf-8").digest("hex");
+console.log("algoritmo sha1:   " + hash2);
+
+//este algoritmo es más seguro que los anteriores
+var hash3 = crypto.createHash("sha256").update(data, "utf-8").digest("hex");
+console.log("algoritmo sha256:   " + hash3);
+
+//este algoritmo es más seguro que los anteriores
+var hash4 = crypto.createHash("sha512").update(data, "utf-8").digest("hex");
+console.log("algoritmo sha512:   " + hash4);
+
